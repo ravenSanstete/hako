fd30434ca8deff62db67fa870ed4e32256232fec
sansmori <omnibos@163.com>
2016-11-18 07:42:25 +0800

    dir name changed, important

---
+++ core/engine/__init__.py
@@ -0,0 +1,14 @@
+"""
+The optimization routine for the hako system.
+
+Which can be a hard work to do. And since a self-organizing prototype may be implemented,
+the responsibility of some supreum optimization macro will be of high value
+
+This is named as engine. It's responsible to organize the runtime env of all the key components
+
+we will first only use tensorflow as its backend
+
+and may use the wrapping layer keras, which, however, doesn't mean that we will not invoke the functions
+of tensorflow
+
+"""
--- core/optimizer/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-The optimization routine for the hako system.
-
-Which can be a hard work to do. And since a self-organizing prototype may be implemented,
-the responsibility of some supreum optimization macro will be of high value
-
-"""
